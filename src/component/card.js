4ee253c3e72a51d0ee049ef1ba4dbee55bb74f2d
faizan542 <faizansilat100@gmail.com>
2022-10-03 17:52:51 +0500

    Created Card

--- src/component/card.js
+++ src/component/card.js
@@ -1,5 +1,5 @@
-function Card(props){
-    return <div style={{height:'100%'}} className='p-3 rounded border '>
+function Card(props) {
+    return <div style={{ height: '100%' }} className='p-3 rounded border'>
         <img src={props.imageSrc} width='100%' alt='' />
         <div className='p-3'>
             <h3>{props.title}</h3>
